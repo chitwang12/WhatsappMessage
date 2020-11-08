88dd598ee75aa96edb469f8a2361fb2062dd56bd
chitwang12 <chitwangandhi12@gmail.com>
2020-11-08 22:51:09 +0530

    2nd commit

--- app/src/main/java/com/example/whatsappmessage/MainActivity.kt
+++ app/src/main/java/com/example/whatsappmessage/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.whatsappmessage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val etNumber:EditText = findViewById(R.id.etNumber)
+        val btnSend : Button = findViewById(R.id.btnSend)
+        btnSend.setOnClickListener()
+        {
+        }
     }
 }
